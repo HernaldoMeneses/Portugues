1673be1c71ca3ec48cfdb85e407ba37249b69182
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-25 15:52:58 -0300

    Update 1.3-Letra.py

--- 1-Fonologia/1.3-Letra.py
+++ 1-Fonologia/1.3-Letra.py
@@ -1,2 +1,18 @@
+be = ['Representação do Som, Símbolo ou Grafia do Som']
+funcao = [
+    'representar fonema',
+    'representar mais de um fonema',
+    'representar ajudar na pronúncia de um fonema',
+]
+
+ex_1 = 'x'
+conj = {
+    'enxame':'x'
+    'exame':'z'
+    'têxtil':'s'
+    'sexo':'ks'
+    }
+
+#pg. ver letras diacríticas
 #pg. 55/1505
 #file:///L:/Linear_/Base-01/A%20Gram%C3%A1tica%20para%20Concursos.pdf
