913ea14e61e9160ec6ea2e7f8a81bcd22e14fca9
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-04-22 11:17:21 -0300

    Create 1-into.py

---
+++ 5-Morfologia/1-into.py
@@ -0,0 +1,25 @@
+"Morfologia é a parte da gramática que estuda a forma dos vocábulos"
+
+Palavras = "Significação própria e existência isolada"
+
+v_livres = ["Substantivos", "adjetivo", "verbo", "alguns advérbios", "numeráis"] # São palavras
+v_dependentes = ["artigo", "conjunção", "preposição", "pronomes átonos"] # Vocábulos Átonos 
+v_presas = ["afixos", "radical", "desinências", "vogal temática"]
+Vocabulos = [v_livres, v_dependentes, v_presasa]
+
+
+Classes = [
+    "Substantivo",
+    "Adjetivo",
+    "Artigo",
+    "Numeral",
+    "Pronome",
+    "Verbo",
+    "Advérbio",
+    "Preposição",
+    "Conjunção",
+    "Interjeição",
+    "P.Denotativas",
+
+]
+
